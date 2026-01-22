6768ef3c933d6737801e0a090ae75da2a4138a97
Headlock Prod <headlock-prod@example.com>
2026-01-22 11:58:41 +0000

    Fix logo alignment and add /watch command

--- Formula/headlock.rb
+++ Formula/headlock.rb
@@ -7,22 +7,22 @@ class Headlock < Formula
   on_macos do
     on_arm do
       url "https://headlock.app/downloads/v0.1.0/headlock-darwin-arm64.tar.gz"
-      sha256 "99c40a9e76d4a955704b9ea72a18a6d8ac46e9a1cb3a7ba0315823f3f5e3d874"
+      sha256 "92987cb3df83e63f1fd0f519a8a1924184be575c8ebb641a613d9c093450c97a"
     end
     on_intel do
       url "https://headlock.app/downloads/v0.1.0/headlock-darwin-amd64.tar.gz"
-      sha256 "447cb30690325723d95ef42e6f3b45bdf3f2b872182c4734910b1cbc4a244bf1"
+      sha256 "5601c5be302fd47cab0b3932d6640647561b75ce36e0474e5d6d7e7883d2e0d2"
     end
   end
 
   on_linux do
     on_arm do
       url "https://headlock.app/downloads/v0.1.0/headlock-linux-arm64.tar.gz"
-      sha256 "1866c150387de98d9bc3ab0adee3df1c006a0379798168d7ccb29c7771255329"
+      sha256 "b1d3073c36d0fbcd243a83017c48e29b2d87061ec210f94e330ecdb55be271ae"
     end
     on_intel do
       url "https://headlock.app/downloads/v0.1.0/headlock-linux-amd64.tar.gz"
-      sha256 "bbc4f49bedb49160f90082332c6ac24a0bdc6153035892c78a4f59f416159a87"
+      sha256 "f4189a68ff49e20239745becabff4482e613a183f7d6c7c5ac62eb1d1a974311"
     end
   end
 
