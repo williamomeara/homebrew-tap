356b9e7060e4954a4efa5c4c598104585c926440
Headlock Prod <headlock-prod@example.com>
2026-01-22 11:50:44 +0000

    Update to v0.1.0 with production API URL

--- Formula/headlock.rb
+++ Formula/headlock.rb
@@ -7,22 +7,22 @@ class Headlock < Formula
   on_macos do
     on_arm do
       url "https://headlock.app/downloads/v0.1.0/headlock-darwin-arm64.tar.gz"
-      sha256 "2fb4748cbc2d2b63129c3b60cc50bcfec64fce2477750694b95946d02b390c04"
+      sha256 "99c40a9e76d4a955704b9ea72a18a6d8ac46e9a1cb3a7ba0315823f3f5e3d874"
     end
     on_intel do
       url "https://headlock.app/downloads/v0.1.0/headlock-darwin-amd64.tar.gz"
-      sha256 "6206f92caafcbcfac97660782a087c38234792e3d5608b77963a43a16ec38e07"
+      sha256 "447cb30690325723d95ef42e6f3b45bdf3f2b872182c4734910b1cbc4a244bf1"
     end
   end
 
   on_linux do
     on_arm do
       url "https://headlock.app/downloads/v0.1.0/headlock-linux-arm64.tar.gz"
-      sha256 "f31bdb7a36471ec9c0b8730f0218efeab0a580f46e32a4deffb2c110bbbae518"
+      sha256 "1866c150387de98d9bc3ab0adee3df1c006a0379798168d7ccb29c7771255329"
     end
     on_intel do
       url "https://headlock.app/downloads/v0.1.0/headlock-linux-amd64.tar.gz"
-      sha256 "d8efb22ec49dcad4d0a8e4dfc0ef9c196b2d30b63d980278d2057184e9a321c5"
+      sha256 "bbc4f49bedb49160f90082332c6ac24a0bdc6153035892c78a4f59f416159a87"
     end
   end
 
