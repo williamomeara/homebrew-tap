8098e1d10807414f760289ac934112944ab55e54
Headlock Prod <headlock-prod@example.com>
2026-01-22 11:39:44 +0000

    Update formula with self-hosted download URLs

--- Formula/headlock.rb
+++ Formula/headlock.rb
@@ -6,23 +6,23 @@ class Headlock < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/williamomeara/headlock/releases/download/v0.1.0/headlock-darwin-arm64.tar.gz"
-      sha256 "TODO_ADD_SHA256_HASH"
+      url "https://headlock.app/downloads/v0.1.0/headlock-darwin-arm64.tar.gz"
+      sha256 "2fb4748cbc2d2b63129c3b60cc50bcfec64fce2477750694b95946d02b390c04"
     end
     on_intel do
-      url "https://github.com/williamomeara/headlock/releases/download/v0.1.0/headlock-darwin-amd64.tar.gz"
-      sha256 "TODO_ADD_SHA256_HASH"
+      url "https://headlock.app/downloads/v0.1.0/headlock-darwin-amd64.tar.gz"
+      sha256 "6206f92caafcbcfac97660782a087c38234792e3d5608b77963a43a16ec38e07"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/williamomeara/headlock/releases/download/v0.1.0/headlock-linux-arm64.tar.gz"
-      sha256 "TODO_ADD_SHA256_HASH"
+      url "https://headlock.app/downloads/v0.1.0/headlock-linux-arm64.tar.gz"
+      sha256 "f31bdb7a36471ec9c0b8730f0218efeab0a580f46e32a4deffb2c110bbbae518"
     end
     on_intel do
-      url "https://github.com/williamomeara/headlock/releases/download/v0.1.0/headlock-linux-amd64.tar.gz"
-      sha256 "TODO_ADD_SHA256_HASH"
+      url "https://headlock.app/downloads/v0.1.0/headlock-linux-amd64.tar.gz"
+      sha256 "d8efb22ec49dcad4d0a8e4dfc0ef9c196b2d30b63d980278d2057184e9a321c5"
     end
   end
 
@@ -31,6 +31,6 @@ class Headlock < Formula
   end
 
   test do
-    assert_match "headlock", shell_output("#{bin}/headlock --version", 2)
+    assert_match "headlock", shell_output("#{bin}/headlock --version 2>&1", 1)
   end
 end
